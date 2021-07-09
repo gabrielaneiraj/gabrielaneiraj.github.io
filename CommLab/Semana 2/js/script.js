e33df92d5584411215d15ec9c54bbb3c95b7f5fc
gabrielaneiraj <86494214+gabrielaneiraj@users.noreply.github.com>
2021-07-09 02:35:49 -0400

    semana 2
    haciendo un comic

---
+++ CommLab/Semana 2/js/script.js
@@ -0,0 +1,9 @@
+alert ("miau miau miau miau miau miau..")
+function cambiacontenido() {
+  document.getElementById('photo').src = "raton.jpg"
+  document.getElementById('photo-caption').innerHT = "ratoncito fallecido"
+}
+function cambiacontenido1() {
+  document.getElementById('photo').src = "galleta.jpg"
+  document.getElementById('photo-caption').innerHT = "galleta chocolate"
+}
