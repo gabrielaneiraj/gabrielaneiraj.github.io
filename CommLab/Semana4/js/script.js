5cf1043913569eff15e59ec56eadd66c170a4a1f
gabrielaneiraj <86494214+gabrielaneiraj@users.noreply.github.com>
2021-08-05 22:00:55 -0400

    SEMANA 4
    video

---
+++ CommLab/Semana4/js/script.js
@@ -0,0 +1,4 @@
+alert ("No olvides poner la alarma")
+// var player es una manera de escribir document.getElementById("player") mas corto
+var player = document.getElementById("player");
+var mensaje = document.getElementById("mensaje");
